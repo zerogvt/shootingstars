ea451554a12f56c8c259fe6a306415bff963e0ab
zerogvt <gkoumplias@gmail.com>
2017-10-25 17:14:15 +0100

    meh

---
+++ garage/parseTextTree.py
@@ -0,0 +1,26 @@
+import sys
+from itertools import takewhile
+
+
+def is_indent(x):
+    return x in ['\t', '|', ' ', '+', '-', '\\', '/']
+
+def build_tree(lines):
+    min_indent = 10000
+    lines = iter(lines)
+    stack = []
+    for line in lines:
+        indent = len(list(takewhile(is_indent, line)))
+        if (indent==0):
+            continue
+        if (indent <= min_indent):
+            min_indent = indent
+            del stack[:]
+        stack.append(line[indent:])
+        if (len(stack)>=2):
+            print(stack[0] + ' >>> ' + stack[-1])
+
+lines=[]
+with open(sys.argv[1]) as f:
+    lines = f.read().splitlines()
+build_tree(lines)
